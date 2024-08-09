9023830e3e3741593b3e66e934e35488085139d4
AmrDroid-git <amrdroidcommunity@gmail.com>
2024-08-09 20:22:29 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '02002da5-919f-44c1-aad5-adac64755b72';
+export const MODAL_CLOSE_SEC = 2.5;
